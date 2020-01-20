a3ac80b4a2a19c7651bfb636f2d6020d86234df9
Joseph-Maulin <59800199+Joseph-Maulin@users.noreply.github.com>
2020-01-20 09:26:07 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Random;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main
+{
+	public static void main(String... args) {
+		/*
+		Animal zebra = new Animal();
+		zebra.setName("zebra");
+		System.out.println(zebra.getName());
+		*/
+		
+		Random r = new Random();
+		System.out.println(r.nextInt(10));
+	}
+}
